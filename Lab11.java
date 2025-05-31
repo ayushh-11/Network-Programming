d6edbc9865c43963db837e1d1a5b308c2439b9f7
Ayush Mahat <ayush.mahat11@gmail.com>
2025-05-31 12:31:49 +0545

    Add solution for Lab11.java

---
+++ Lab11.java
@@ -0,0 +1,25 @@
+//Write a program to split URL.
+
+import java.net.URL;
+public class Lab11 {
+    public void getDetails(String s){
+        try{
+            URL u = new URL(s);
+            System.out.println("URL is " + u.toString());
+            System.out.println("Port is " + u.getPort());
+            System.out.println("Protocol is " + u.getProtocol());
+            System.out.println("Host is " + u.getHost());
+            System.out.println("File is " + u.getFile());
+            System.out.println("Authority is " + u.getAuthority());
+            System.out.println("Query is " + u.getQuery()); 
+            System.out.println("Default is " + u.getDefaultPort());           
+        }
+        
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+        new Lab11().getDetails("https://www.facebook.com");
+    }
+}
