2cc47a7f2acaf166bf783059afec1672daada838
Ayush Mahat <108381214+ayushh-11@users.noreply.github.com>
2025-06-02 15:36:29 +0545

    Update CheckIP.java

--- CheckIP.java
+++ CheckIP.java
@@ -7,21 +7,16 @@ import java.net.UnknownHostException;
 
 public class CheckIP {
     public static void main(String[] args) {
-        // Replace these with any IP address you want to test
-        String ipv4Address = "192.168.0.1";
-        String ipv6Address = "2001:db8::1";
+        
+        String ipAddress = "192.168.0.1";
 
-        System.out.println("Checking IPv4 Address:");
-        checkIPType(ipv4Address);
-
-        System.out.println("\nChecking IPv6 Address:");
-        checkIPType(ipv6Address);
+        System.out.println("Checking Address:");
+        checkIPType(ipAddress);
     }
 
     public static void checkIPType(String ip) {
         try {
             InetAddress address = InetAddress.getByName(ip);
-
             if (address instanceof Inet4Address) {
                 System.out.println(ip + " is a valid IPv4 address.");
             } else if (address instanceof Inet6Address) {
