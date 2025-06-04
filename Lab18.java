28a2ff558f6218e11e014cce941cb81c58cbc29c
Ayush Mahat <ayush.mahat11@gmail.com>
2025-06-04 12:57:43 +0545

    Add solution for Lab18.java

---
+++ Lab18.java
@@ -0,0 +1,17 @@
+//Write a program to retrieve cookies information.
+import java.net.*;
+import java.util.List;
+
+public class Lab18 {
+    public static void main(String[] args) throws Exception {
+        CookieHandler.setDefault(new CookieManager());
+        URL url = new URL("https://www.facebook.com");
+        URLConnection connection = url.openConnection();
+        connection.getContent();
+        List<HttpCookie> cookies = ((CookieManager) CookieHandler.getDefault()).getCookieStore().getCookies();
+        for (HttpCookie cookie : cookies) {
+            System.out.println("Cookie: " + cookie.getName() + " = " + cookie.getValue());
+        }
+    }
+
+}
