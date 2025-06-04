81899eceab276f3ebd3ce8b29d66106fad720163
Ayush Mahat <ayush.mahat11@gmail.com>
2025-06-04 12:57:46 +0545

    Add solution for Lab19.java

---
+++ Lab19.java
@@ -0,0 +1,19 @@
+//Write a program to retrieve information from header using header specific methods.
+
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+
+public class Lab19 {
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        URL u = new URL("http://www.tu.edu.np");
+        URLConnection uc = u.openConnection();
+        System.out.println("Content-Type:" + uc.getContentType());
+        System.out.println("Content-Length:" + uc.getContentLength());
+        System.out.println("Content-Encoding:" + uc.getContentEncoding());
+        System.out.println("Date:" + new Date(uc.getDate()));
+        System.out.println("Expiry Date:" + new Date(uc.getExpiration()));
+        System.out.println("Last modified Date:" + new Date(uc.getLastModified()));
+
+    }
+}
