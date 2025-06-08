585c319a354bf35457a45ce6c1b601decdf96af3
Ayush Mahat <ayush.mahat11@gmail.com>
2025-06-08 15:57:35 +0545

    Add solution for Lab25.java

---
+++ Lab25.java
@@ -0,0 +1,17 @@
+
+import java.net.Socket;
+
+public class Lab25 {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("www.facebook.com", 80);
+            System.out.println("Remote Address: " + socket.getInetAddress());
+            System.out.println("Remote Port: " + socket.getPort());
+            System.out.println("Local Address: " + socket.getLocalAddress());
+            System.out.println("Local Port: " + socket.getLocalPort());
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
