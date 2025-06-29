06d1f794ccd88601fa3baf5852dbc32485f3c18d
Ayush Mahat <108381214+ayushh-11@users.noreply.github.com>
2025-06-29 13:17:43 +0545

    Update Lab12.java

--- Lab12.java
+++ Lab12.java
@@ -1,41 +1,19 @@
-//Write a program to check which protocol does a virtual machine support?
-import java.net.*;
+import java.net.URL;
 
-public class Lab12 {
+public class ProtocolSupportChecker {
     public static void main(String[] args) {
-        try {
-            String interfaceName = "ethernet_3";// change to your interface name
-            NetworkInterface netIf = NetworkInterface.getByName(interfaceName);
+        String[] protocols = {
+            "http", "https", "ftp", "file", "jar", "mailto", "gopher", "nfs", "jdbc"
+        };
 
-            if (netIf == null) {
-                System.out.println("Interface " + interfaceName + " not found.");
-                return;
+        for (String protocol : protocols) {
+            try {
+                URL url = new URL(protocol + "://example.com");
+                url.openConnection(); // Try to open a connection
+                System.out.println(protocol + " is supported.");
+            } catch (Exception e) {
+                System.out.println(protocol + " is NOT supported.");
             }
-
-            System.out.println("Interface: " + netIf.getName());
-
-            if (!netIf.isUp()) {
-                System.out.println("Status: DOWN");
-                return;
-            } else {
-                System.out.println("Status: UP");
-            }
-
-            System.out.println("Supports Multicast: " + netIf.supportsMulticast());
-
-            boolean supportsIPv4 = false;
-            boolean supportsIPv6 = false;
-
-            for (InetAddress addr : java.util.Collections.list(netIf.getInetAddresses())) {
-                if (addr instanceof Inet4Address) supportsIPv4 = true;
-                if (addr instanceof Inet6Address) supportsIPv6 = true;
-            }
-
-            System.out.println("Supports IPv4: " + supportsIPv4);
-            System.out.println("Supports IPv6: " + supportsIPv6);
-
-        } catch (SocketException e) {
-            System.out.println("Error: " + e.getMessage());
         }
     }
 }
